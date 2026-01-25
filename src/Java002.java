edc9b6c6ba493d0d15abf0b40bc233c43a5496f7
Yedhu-krishnan-PS <yedhuskrishnan@gmail.com>
2026-01-25 23:07:53 +0530

    first push

--- src/Java002.java
+++ src/Java002.java
@@ -7,7 +7,7 @@ public class Java002 {
         System.out.println(8 + 9);
         System.out.println(9 * 8);
         System.out.println(99 * 89);
-        System.out.println(78*23);
+        System.out.println(78*22);
         System.out.println(84/21);
     }
 }
